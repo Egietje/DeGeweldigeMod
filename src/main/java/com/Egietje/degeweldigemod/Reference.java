8f4e4a0954827cf3823005fe5b9b7eabc25ea912
Egietje <kokkie7beer@gmail.com>
2016-07-24 11:31:09 +0200

    Updated to V2.5

--- src/main/java/com/Egietje/degeweldigemod/Reference.java
+++ src/main/java/com/Egietje/degeweldigemod/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	
 	public static final String MODID = "dgm";
 	public static final String NAME = "§6De§r§eGeweldige§r§6Mod§r";
-	public static final String VERSION = "2.4.1";
+	public static final String VERSION = "2.5";
 	public static final String CLIENTPROXY = "com.Egietje.degeweldigemod.proxy.ClientProxy";
 	public static final String COMMONPROXY = "com.Egietje.degeweldigemod.proxy.CommonProxy";
 	
